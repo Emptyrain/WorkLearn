df5b0468b3e7f148db1f956719134e4650a8fdde
Emptyrain <916954352@qq.com>
2021-06-28 16:34:26 +0800

    简单的英文词频统计，并输出前10

---
+++ Spark/SparkWordCount.py
@@ -0,0 +1,17 @@
+#-# -*- coding = utf-8 -*-
+# @time:2021/6/28 15:48
+# Author:Rain
+# @File:SparkWordCount.py
+# @Software:PyCharm
+
+import findspark
+findspark.init()
+from pyspark import SparkContext
+
+sc = SparkContext('local[*]','WordCount')
+Data = sc.textFile('哈利波特.txt')
+RDD = Data.flatMap(lambda x:x.split(' ')).map(lambda x:(x,1)).reduceByKey(lambda x,y:x+y)\
+    .sortBy(lambda x:x[1],ascending=False).take(10)
+print(RDD)
+
+
